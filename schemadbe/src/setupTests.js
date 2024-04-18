68700cc96093216b4c24f63ab5ef9dab02fb9d2c
sneha6129 <165042487+sneha6129@users.noreply.github.com>
2024-04-18 11:16:29 +0530

    Update setupTests.js

--- schemadbe/src/setupTests.js
+++ schemadbe/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+//Invoke-RestMethod -Method Post -Uri "http://localhost:5000/api/users" -ContentType "application/json" -Body '{"name": "John Doe", "email": "john@example.com", "age": 30}'
+//Invoke-RestMethod -Method Post -Uri "http://localhost:5000/api/users" -ContentType "application/json" -Body '{"name": "Alex", "email": "john@yahoo.com", "age": 35}' 
+//Invoke-RestMethod -Method Get -Uri "http://localhost:5000/api/users"
+//Invoke-RestMethod -Method Get -Uri "http://localhost:5000/api/users/65cb829decab01d45546620b" (65cb829decab01d45546620b--userID in DB)
+//Invoke-RestMethod -Method Put -Uri "http://localhost:5000/api/users/65cb829decab01d45546620b" -ContentType "application/json" -Body '{"name": "Siri", "email": "siri@in.com", "age": 25}'  
+//Invoke-RestMethod -Method Delete -Uri "http://localhost:5000/api/users/65cb821aecab01d455466209"
